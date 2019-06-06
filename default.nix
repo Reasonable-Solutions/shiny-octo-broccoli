e34b5e2a9c306f15d2707efa6c25613178abfbb0
Carl Hedgren <carl.hedgren@creuna.no>
2019-06-06 16:06:55 +0200

    simplify

--- default.nix
+++ default.nix
@@ -1,16 +1,14 @@
-
-let defaults = { config, pkgs, name, ... }:
 {
-  imports = [
-    ./basic-virtualbox.nix
-    ./service.nix
-  ];
-};
-
-in {
+  network.enableRollback = true;
   network.description = "misc services";
+
+  defaults ={
+    imports = [
+      ./basic-virtualbox.nix
+    ];
+  };
+
   web = { pkgs, config, ... }: {
-    imports = [ defaults ];
     deployment.targetHost = "169.254.92.92";
     networking.hostName = "cloudLike";
     networking.firewall.allowedTCPPorts = [ 80 443 ];
